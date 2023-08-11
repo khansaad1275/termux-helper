23304a80422b9abcbd13e8bf4ceeb0e2d4425b8a
Saad Maqsood <32749921+khansaad1275@users.noreply.github.com>
2023-08-12 00:59:49 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+directory_path="$PREFIX/termux-helper"
+
+if [ -d "$directory_path" ]; then
+    echo "The directory '$directory_path' exists."
+    echo "#- " >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    echo "#-------------TERMUX-HELPER---------------" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    echo "alias learn='bash \$PREFIX/termux-helper/termuxhelper.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    echo "alias learn='bash \$PREFIX/termux-helper/termuxhelper.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    echo "echo -e '\033[1;37;42mType learn to open termux-helper \033[0m'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+
+    
+else
+    echo "The directory '$directory_path' does not exist."
+    pkg install git || cd $PREFIX || git clone https://github.com/khansaad1275/termux-helper/ || cd termux-helper || chmod +x learntermux_menu.sh || bash install.sh
+
+fi
