0db6e103252674dfaad6a9783e9ba8f715577664
Saad Maqsood <32749921+khansaad1275@users.noreply.github.com>
2023-08-13 01:12:47 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 sed '/----TERMUX-HELPER-------/,/-----END-----/d' /data/data/com.termux/files/usr/etc/bash.bashrc > temp.bashrc && mv temp.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
-rm -rf $PREFIX/termux-helper
-cd 
+rm -rf $PREFIX/termux-helper 
 clear
 echo -e "\033[1;37;42mTermux Helper has been uninstalled from your termux, You can install it from Learntermux.tech any time. <3 \033[0m"
 termux-reload-settings
+cd
 
