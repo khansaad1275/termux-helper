8c58e12be053c7375ff278154431e210e7ad1ce6
Saad Maqsood <32749921+khansaad1275@users.noreply.github.com>
2023-08-12 15:58:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,11 @@ if [ -d "$directory_path" ]; then
     echo "#-------------TERMUX-HELPER---------------" >> /data/data/com.termux/files/usr/etc/bash.bashrc
     echo "alias learn='bash \$PREFIX/termux-helper/termuxhelper.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
     echo "alias learn='bash \$PREFIX/termux-helper/termuxhelper.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-    echo "alias l='bash \$PREFIX/termux-helper/termuxhelper.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-    echo "echo -e '\033[1;37;42m>>> Type learn or l press Enter to open termux-helper <3 \033[0m'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    
+    # This is bugging if i type ls wrong
+    #echo "alias l='bash \$PREFIX/termux-helper/termuxhelper.sh'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+    
+    echo "echo -e '\033[1;37;42m>>> Type learn and press Enter to open termux-helper <3 \033[0m'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
     echo "echo '' " >> /data/data/com.termux/files/usr/etc/bash.bashrc
     echo "#-------------------END-------------------" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
@@ -17,3 +20,5 @@ else
     pkg install git || cd $PREFIX || git clone https://github.com/khansaad1275/termux-helper/ || cd termux-helper || chmod +x learntermux_menu.sh || bash install.sh
 
 fi
+termux-reload-settings
+echo -e '\033[1;37;42mTermux-helper Installed <3 \033[0m'
